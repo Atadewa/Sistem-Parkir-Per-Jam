8823516a4328945a9fcd5d13c546e747d85c269b
Ericharw <ericharizki12@gmail.com>
2023-11-26 14:06:13 +0700

    feat: menambahkan perhitungan matematika

--- SistemParkir.java
+++ SistemParkir.java
@@ -4,8 +4,16 @@ public class SistemParkir {
         Scanner input = new Scanner (System.in);
 
         //Deklarasi Variabel
-        String jenisKendaraan, gedung, exit;
-        int idMasuk, tarifParkirAwal, tarifHelm, waktuParkir, totalPembayaran, pembayaran;
+        String isMember, jenisKendaraan, kode, gedung, exit;
+        int idMasuk, tarifParkirAwal, tarifHelm, totalSementara, waktuParkir,tarifParkirPerjam, tarifParkirTotal, totalPembayaran, pembayaran, kembalian;
+        float diskon, totalDiskon;
+
+        //User menginputkan member atau bukan
+        System.out.print("\nAnda pengguna umum atau member\t: ");
+        isMember = input.nextLine();
+        System.out.println("Jika anda member, masukkan 0.1; jika bukan, ,masukkan 0");
+        System.out.print("Diskon\t\t\t\t:");
+        diskon = input.nextFloat();
 
         //User menginputkan nomor polisi, jenis kendaraaan, dan gedung parkir
         System.out.print("Masukkan ID sesuai nomor polisi\t: ");
@@ -25,19 +33,40 @@ public class SistemParkir {
         System.out.print("Jawaban\t\t\t\t: ");
         tarifHelm = input.nextInt();
 
-        //Output total Sementara
-        System.out.println("Total Sementara : ");
+        //Pembuatan kode
+        kode = "2939"+gedung+idMasuk;
+        totalSementara = tarifParkirAwal+tarifHelm;
+
+        //User keluar parkiran 
+        System.out.print("\nKetik 'Exit' jika ingin keluar parkiran : ");
+        input.nextLine();
+        exit = input.nextLine();
+
+        //Output Nomor struk dan total Sementara
+        System.out.println("Nomor struk : " + kode);
+        System.out.println("Total Sementara : "+totalSementara);
         
-        //User menginputkan waktu parkir
+        //User menginputkan kode dan waktu parkir
+        System.out.print("Masukkan nomor struk Anda\t: ");
+        kode = input.nextLine();
         System.out.print("Waktu Anda parkir (jam) \t: ");
         waktuParkir = input.nextInt();
 
-        //Output total pembayaran 
-        System.out.println("Total Pembayaran : ");
+        //Perhitungan tarif total
+        tarifParkirPerjam = (waktuParkir-1)*1000;
+        tarifParkirTotal = tarifParkirAwal + tarifParkirPerjam;
+        totalPembayaran = tarifParkirTotal + tarifHelm;
+        totalDiskon = totalPembayaran*diskon;
+        totalPembayaran -=totalDiskon;
 
-        //User melakukan pembayaran
+        //Output total pembayaran 
+        System.out.println("Total Pembayaran : "+totalPembayaran);
+
+        //User melakukan pembayaran dan menerima kembalian 
         System.out.print("\nMasukkan nilai yang harus Anda bayar : ");
-        pembayaran = input.nextInt();        
+        pembayaran = input.nextInt();   
+        kembalian = pembayaran-totalPembayaran;
+        System.out.println("Kembalian : "+kembalian);  
 
     }
 }
